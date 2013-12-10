a1203f49a2417411979865b9e51e70d1c998382e
Ali Al Dallal <ali@alicoding.com>
2013-12-10 18:14:08 -0500

    Reindent

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var hyperquest = require('hyperquest'),
-    fs = require('fs'),
+  fs = require('fs'),
     xml2js = require('xml2js');
 
 function _parse_json(callback) {
@@ -9,23 +9,23 @@ function _parse_json(callback) {
       if (err) {
             return callback(err);
           }
-
+  
       var bodyParts = [];
       var bytes = 0;
-      res.on("data", function(c) {
+      res.on("data", function (c) {
             bodyParts.push(c);
             bytes += c.length;
           });
-      res.on("end", function() {
-        var json, body;
-
+      res.on("end", function () {
+            var json, body;
+      
             try {
-              body = Buffer.concat(bodyParts, bytes).toString("utf8");
-              json = JSON.parse(body);
-            } catch (ex) {
-              console.log(body);
-              return callback(ex);
-            }
+                    body = Buffer.concat(bodyParts, bytes).toString("utf8");
+                    json = JSON.parse(body);
+                  } catch (ex) {
+                          console.log(body);
+                          return callback(ex);
+                        }
             callback(null, json);
           });
       res.on("error", callback);
@@ -36,6 +36,7 @@ function request(callback) {
   hyperquest.get("http://transifex.webmaker.org/popcorn/th_TH/popcorn.webmaker.org.json", _parse_json(callback));
 }
 
-request(function(err, body) {
+request(function (err, body) {
   console.log(err, body);
 });
+
