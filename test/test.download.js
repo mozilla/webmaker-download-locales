fdf49e6b824a22434648cbfd1c161ee8e17f86c5
Kate Hudson <k88hudson@gmail.com>
2014-09-10 15:50:53 -0400

    Fix tests

--- test/test.download.js
+++ test/test.download.js
@@ -16,21 +16,20 @@ var apps = [
 describe("download-webmaker-locales", function() {
   apps.forEach(function(app) {
     var tmp = require("os").tmpdir() + "/webmaker-download-locales/" + app;
-
     it("should download fr, en-US locale for " + app, function(done) {
       downloader({app: app, dir: tmp, languages: ['fr', 'en-US']}, function(err) {
         should.not.exist(err);
         done();
       });
     });
+  });
 
-    it("should download all locales for mobile-appmaker", function(done) {
-      this.timeout = 5000;
-      downloader({app: 'mobile-appmaker', dir: tmp}, function(err) {
-        should.not.exist(err);
-        done();
-      });
+  it("should download all locales for mobile-appmaker", function(done) {
+    var tmp = require("os").tmpdir() + "/webmaker-download-locales/mobile-appmaker";
+    this.timeout = 5000;
+    downloader({app: 'mobile-appmaker', dir: tmp}, function(err) {
+      should.not.exist(err);
+      done();
     });
-
   });
 });
