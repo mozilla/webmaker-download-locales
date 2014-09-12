0da435d61bdba06a28541897e3525585630a1e38
Kate Hudson <k88hudson@gmail.com>
2014-09-12 12:47:25 -0400

    Use / as a separator for urls

--- webmaker-download-locales.js
+++ webmaker-download-locales.js
@@ -12,7 +12,7 @@ module.exports = function(options, callback) {
     }
 
     var q = async.queue(function(translation, callback) {
-      var local_path_split = url.parse(translation).pathname.split(path.sep);
+      var local_path_split = url.parse(translation).pathname.split("/");
       var local_path = path.join(options.dir, local_path_split[2], local_path_split[3]);
 
       utils.stream_url_to_file(translation, local_path, callback);
